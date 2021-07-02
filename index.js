6a0f44696ba6984c1ed30a66cab29e3696ba7d60
Bonny-code <ndiboniface@outlook.com>
2021-07-02 14:43:35 +0100

    js almsot done

---
+++ index.js
@@ -0,0 +1,17 @@
+let x = "john"
+let y = "Doe"
+let z = x + y
+console.log(z)
+
+let person = {
+    name: "Boni",
+    surname: "Ndi",
+    email: "ndibonny@gmail.com"
+}
+console.log(person)
+
+delete person.email
+console.log(person)
+
+let man = ["janauary", "february", "march", "april", "may", "june", "july", "august", "september", "october"]
+console.log(man)
